6abf0a31b7378b1413caf3a932d9dc3ba4dd4fdb
panchenkodv <lexx.klan@mail.ru>
2017-08-27 23:50:55 +0300

    initial commit

---
+++ frontend/views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Здесь будет твой сайт!</h1>
+
+        <p class="lead">Я люблю тебя и верю, что у тебя все получится! Я помогу тебе во всем!</p>
+    </div>
+</div>
